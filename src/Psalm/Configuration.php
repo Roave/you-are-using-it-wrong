42c5a9c8ed77f6ada2cf6340d2e0387699dacace
Marco Pivetta <ocramius@gmail.com>
2019-05-04 04:25:06 +0200

    Created a bridge Psalm configuration that filters issues by namespaces
    We only check namespaces from packages that have `roave/you-are-using-it-wrong`
    as dependency, so this Psalm configuration will provide filtering of reported
    issues to just these namespaces.

---
+++ src/Psalm/Configuration.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Roave\YouAreUsingItWrong\Psalm;
+
+use Psalm\Config as PsalmConfig;
+use Psalm\Config\ProjectFileFilter;
+use Psalm\Issue\ArgumentIssue;
+use Psalm\Issue\ClassIssue;
+use Psalm\Issue\CodeIssue;
+use Psalm\Issue\FunctionIssue;
+use Psalm\Issue\MethodIssue;
+use Psalm\Issue\PropertyIssue;
+
+/** @internal this class is only for configuring psalm according to the defaults of this repository */
+final class Configuration extends PsalmConfig
+{
+    /** @var string[] */
+    private $checkedNamespaces;
+
+    protected function __construct(ProjectFileFilter $files, string ...$checkedNamespaces)
+    {
+        parent::__construct();
+
+        $this->project_files           = $files;
+        $this->allow_phpstorm_generics = true;
+        $this->use_docblock_types      = true;
+        $this->totally_typed           = true;
+        $this->checkedNamespaces       = $checkedNamespaces;
+    }
+
+    public static function forStrictlyCheckedNamespacesAndProjectFiles(
+        ProjectFileFilter $projectFileFilter,
+        string ...$namespaces
+    ) {
+        return new self($projectFileFilter, ...$namespaces);
+    }
+
+    public function getReportingLevelForIssue(CodeIssue $e, array $suppressed_issues = []) : string
+    {
+        if (
+            ($e instanceof ClassIssue && $this->identifierMatchesNamespace($e->fq_classlike_name))
+            || ($e instanceof PropertyIssue && $this->identifierMatchesNamespace($e->property_id))
+            || ($e instanceof MethodIssue && $this->identifierMatchesNamespace($e->method_id))
+            || (
+                ($e instanceof FunctionIssue || $e instanceof ArgumentIssue)
+                && $this->identifierMatchesNamespace($e->function_id)
+            )
+        ) {
+            return parent::getReportingLevelForIssue($e, $suppressed_issues);
+        }
+
+        return self::REPORT_SUPPRESS;
+    }
+
+    private function identifierMatchesNamespace(string $identifier) : bool
+    {
+        foreach ($this->checkedNamespaces as $namespace) {
+            if (stripos($identifier, $namespace) === 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ test/unit/Psalm/ConfigurationTest.php
@@ -0,0 +1,199 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RoaveTest\YouAreUsingItWrong\Psalm;
+
+use PHPUnit\Framework\TestCase;
+use Psalm\Config\ProjectFileFilter;
+use Psalm\Issue\ArgumentIssue;
+use Psalm\Issue\ClassIssue;
+use Psalm\Issue\CodeIssue;
+use Psalm\Issue\FunctionIssue;
+use Psalm\Issue\MethodIssue;
+use Psalm\Issue\PropertyIssue;
+use Roave\YouAreUsingItWrong\Psalm\Configuration;
+
+/** @covers \Roave\YouAreUsingItWrong\Psalm\Configuration */
+final class ConfigurationTest extends TestCase
+{
+    public function testConfigurationDefaults() : void
+    {
+        $reflectionFiles            = new \ReflectionProperty(Configuration::class, 'project_files');
+        $reflectionPhpstormGenerics = new \ReflectionProperty(Configuration::class, 'allow_phpstorm_generics');
+        $reflectionUseDocblockTypes = new \ReflectionProperty(Configuration::class, 'use_docblock_types');
+        $reflectionTotallyTyped     = new \ReflectionProperty(Configuration::class, 'totally_typed');
+
+        $reflectionFiles->setAccessible(true);
+        $reflectionPhpstormGenerics->setAccessible(true);
+        $reflectionUseDocblockTypes->setAccessible(true);
+        $reflectionTotallyTyped->setAccessible(true);
+
+        $projectFiles  = $this->createMock(ProjectFileFilter::class);
+        $configuration = Configuration::forStrictlyCheckedNamespacesAndProjectFiles($projectFiles);
+
+        self::assertSame($projectFiles, $reflectionFiles->getValue($configuration));
+        self::assertTrue($reflectionPhpstormGenerics->getValue($configuration));
+        self::assertTrue($reflectionUseDocblockTypes->getValue($configuration));
+        self::assertTrue($reflectionTotallyTyped->getValue($configuration));
+    }
+
+    /** @dataProvider expectedReportingLevels */
+    public function testCheckedNamespaces(
+        CodeIssue $issue,
+        string $expectedReportingLevel,
+        string ...$checkedNamespaces
+    ) : void {
+        $projectFiles = $this->createMock(ProjectFileFilter::class);
+
+        self::assertSame(
+            $expectedReportingLevel,
+            Configuration
+                ::forStrictlyCheckedNamespacesAndProjectFiles($projectFiles, ...$checkedNamespaces)
+                ->getReportingLevelForIssue($issue)
+        );
+    }
+
+    public function expectedReportingLevels() : array
+    {
+        $classIssue    = $this->createMock(ClassIssue::class);
+        $propertyIssue = $this->createMock(PropertyIssue::class);
+        $methodIssue   = $this->createMock(MethodIssue::class);
+        $functionIssue = $this->createMock(FunctionIssue::class);
+        $argumentIssue = $this->createMock(ArgumentIssue::class);
+
+        $classIssue->fq_classlike_name = 'Foo\\Bar\\Baz';
+        $propertyIssue->property_id    = 'Foo\\Bar\\Baz$property';
+        $methodIssue->method_id        = 'Foo\\Bar\\Baz::method';
+        $functionIssue->function_id    = 'Foo\\Bar\\Baz\\function_name';
+        $argumentIssue->function_id    = 'Foo\\Bar\\Baz\\function_name';
+
+        return [
+            'no namespaces, class issue'                           => [
+                $classIssue,
+                Configuration::REPORT_SUPPRESS,
+            ],
+            'non-matching namespaces, class issue'                 => [
+                $classIssue,
+                Configuration::REPORT_SUPPRESS,
+                'AAA\\BBB',
+                'AAA\\BBB\\',
+                'Bar\\',
+            ],
+            'matching namespaces, class issue'                     => [
+                $classIssue,
+                Configuration::REPORT_ERROR,
+                'AAA\\BBB',
+                'AAA\\BBB\\',
+                'Foo\\Bar\\',
+            ],
+            'case-insensitive matching namespaces, class issue'    => [
+                $classIssue,
+                Configuration::REPORT_ERROR,
+                'AAA\\BBB',
+                'AAA\\BBB\\',
+                'foo\\',
+            ],
+            'no namespaces, property issue'                        => [
+                $propertyIssue,
+                Configuration::REPORT_SUPPRESS,
+            ],
+            'non-matching namespaces, property issue'              => [
+                $propertyIssue,
+                Configuration::REPORT_SUPPRESS,
+                'AAA\\BBB',
+                'AAA\\BBB\\',
+                'Bar\\',
+            ],
+            'matching namespaces, property issue'                  => [
+                $propertyIssue,
+                Configuration::REPORT_ERROR,
+                'AAA\\BBB',
+                'AAA\\BBB\\',
+                'Foo\\Bar\\',
+            ],
+            'case-insensitive matching namespaces, property issue' => [
+                $propertyIssue,
+                Configuration::REPORT_ERROR,
+                'AAA\\BBB',
+                'AAA\\BBB\\',
+                'foo\\',
+            ],
+            'no namespaces, method issue'                          => [
+                $methodIssue,
+                Configuration::REPORT_SUPPRESS,
+            ],
+            'non-matching namespaces, method issue'                => [
+                $methodIssue,
+                Configuration::REPORT_SUPPRESS,
+                'AAA\\BBB',
+                'AAA\\BBB\\',
+                'Bar\\',
+            ],
+            'matching namespaces, method issue'                    => [
+                $methodIssue,
+                Configuration::REPORT_ERROR,
+                'AAA\\BBB',
+                'AAA\\BBB\\',
+                'Foo\\Bar\\',
+            ],
+            'case-insensitive matching namespaces, method issue'   => [
+                $methodIssue,
+                Configuration::REPORT_ERROR,
+                'AAA\\BBB',
+                'AAA\\BBB\\',
+                'foo\\',
+            ],
+            'no namespaces, function issue'                        => [
+                $functionIssue,
+                Configuration::REPORT_SUPPRESS,
+            ],
+            'non-matching namespaces, function issue'              => [
+                $functionIssue,
+                Configuration::REPORT_SUPPRESS,
+                'AAA\\BBB',
+                'AAA\\BBB\\',
+                'Bar\\',
+            ],
+            'matching namespaces, function issue'                  => [
+                $functionIssue,
+                Configuration::REPORT_ERROR,
+                'AAA\\BBB',
+                'AAA\\BBB\\',
+                'Foo\\Bar\\',
+            ],
+            'case-insensitive matching namespaces, function issue' => [
+                $functionIssue,
+                Configuration::REPORT_ERROR,
+                'AAA\\BBB',
+                'AAA\\BBB\\',
+                'foo\\',
+            ],
+            'no namespaces, argument issue'                        => [
+                $argumentIssue,
+                Configuration::REPORT_SUPPRESS,
+            ],
+            'non-matching namespaces, argument issue'              => [
+                $argumentIssue,
+                Configuration::REPORT_SUPPRESS,
+                'AAA\\BBB',
+                'AAA\\BBB\\',
+                'Bar\\',
+            ],
+            'matching namespaces, argument issue'                  => [
+                $argumentIssue,
+                Configuration::REPORT_ERROR,
+                'AAA\\BBB',
+                'AAA\\BBB\\',
+                'Foo\\Bar\\',
+            ],
+            'case-insensitive matching namespaces, argument issue' => [
+                $argumentIssue,
+                Configuration::REPORT_ERROR,
+                'AAA\\BBB',
+                'AAA\\BBB\\',
+                'foo\\',
+            ],
+        ];
+    }
+}
