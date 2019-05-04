a3078855120df3594fc82705e135e4f60dbf0b04
Marco Pivetta <ocramius@gmail.com>
2019-05-04 04:28:01 +0200

    Implemented the "Hook" that will start the type checks and fail on errors
    Currently, this leads to a hard process execution halt in case of found
    type errors in selected dependencies. This will do, for now.

---
+++ src/Hook.php
@@ -0,0 +1,101 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Roave\YouAreUsingItWrong;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Package\Locker;
+use Composer\Package\RootPackageInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
+use PackageVersions\Versions;
+use Psalm\Config\ProjectFileFilter;
+use Psalm\Internal\Analyzer\ProjectAnalyzer;
+use Psalm\Internal\Provider\FileProvider;
+use Psalm\Internal\Provider\Providers;
+use Psalm\IssueBuffer;
+use Roave\YouAreUsingItWrong\Composer\PackageAutoload;
+use Roave\YouAreUsingItWrong\Composer\PackagesRequiringStrictChecks;
+use Roave\YouAreUsingItWrong\Composer\Project;
+use Roave\YouAreUsingItWrong\Psalm\Configuration;
+use Roave\YouAreUsingItWrong\Psalm\ProjectFilesToBeTypeChecked;
+use RuntimeException;
+use function array_merge;
+
+/** @internal this is a composer plugin: do not rely on it in your sources */
+final class Hook implements PluginInterface, EventSubscriberInterface
+{
+    private const THIS_PACKAGE_NAME = 'roave/enforce-type-checks';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function activate(Composer $composer, IOInterface $io) : void
+    {
+        // Nothing to do here, as all features are provided through event listeners
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function getSubscribedEvents() : array
+    {
+        return [
+            ScriptEvents::POST_INSTALL_CMD => 'runTypeChecks',
+            ScriptEvents::POST_UPDATE_CMD  => 'runTypeChecks',
+        ];
+    }
+
+    /**
+     * @throws RuntimeException
+     */
+    public static function runTypeChecks(Event $composerEvent) : void
+    {
+        $io       = $composerEvent->getIO();
+        $composer = $composerEvent->getComposer();
+        $project  = Project::fromComposerInstallationContext($composer->getPackage(), $composer->getLocker(), getcwd());
+
+        if (! $project->strictTypeChecksAreEnforcedByLocalInstallation()) {
+            $io->write('<info>' . self::THIS_PACKAGE_NAME . ':</info> plugin not installed locally - skipping type checks...');
+
+            return;
+        }
+
+        $io->write('<info>' . self::THIS_PACKAGE_NAME . ':</info> checking strictly type-checked packages...');
+
+        self::analyseProject($project);
+
+        $io->write('<info>' . self::THIS_PACKAGE_NAME . ':</info> ... done checking strictly type-checked packages');
+    }
+
+    private static function analyseProject(Project $project) : void
+    {
+        defined('PSALM_VERSION') || define('PSALM_VERSION', Versions::getVersion('vimeo/psalm'));
+        defined('PHP_PARSER_VERSION') || define('PHP_PARSER_VERSION', Versions::getVersion('nikic/php-parser'));
+
+        // At this stage of the installation, project dependencies are not yet autoloadable
+        require_once getcwd() . '/vendor/autoload.php';
+
+        $startTime = microtime(true);
+        // @TODO in project with psalm config, skip analysis: these people know what they are doing.
+
+        $files           = ProjectFilesToBeTypeChecked::fromAutoloadDefinitions($project->rootPackageAutoload());
+        $config          = Configuration::forStrictlyCheckedNamespacesAndProjectFiles(
+            $files,
+            ...$project
+            ->packagesRequiringStrictTypeChecks()
+            ->namespacesForWhichUsagesAreToBeTypeChecked()
+        );
+        $projectAnalyzer = new ProjectAnalyzer($config, new Providers(new FileProvider));
+
+        $config->visitComposerAutoloadFiles($projectAnalyzer);
+        $projectAnalyzer->check(__DIR__, false);
+
+        // NOTE: this calls exit(1) on failed checks - currently not a problem, but it may become one
+        IssueBuffer::finish($projectAnalyzer, true, $startTime);
+    }
+}
---
+++ test/unit/TestHook.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RoaveTest\YouAreUsingItWrong;
+
+use PHPUnit\Framework\TestCase;
+use Roave\YouAreUsingItWrong\Hook;
+
+/** @covers \Roave\YouAreUsingItWrong\Hook */
+final class TestHook extends TestCase
+{
+    public function testSubscribedEvents() : void
+    {
+        $subscribers = Hook::getSubscribedEvents();
+
+        self::assertSame(['post-install-cmd', 'post-update-cmd'], array_keys($subscribers));
+
+        foreach ($subscribers as $subscriber) {
+            self::assertIsCallable([Hook::class, $subscriber]);
+        }
+    }
+}
